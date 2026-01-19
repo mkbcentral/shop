77c8e00872533ca2352c1238948900bb5cbb46ed
mkbcentral <mkbcentralcode@gmail.com>
2026-01-19 23:44:13 +0200

    chore: Ignorer les fichiers de cache de vues Laravel

--- storage/framework/views/6c7b06b8b0fb797051574ddd42233afa.php
+++
@@ -1,5 +0,0 @@
-<tbody class="bg-white divide-y divide-gray-200">
-    <?php echo e($slot); ?>
-
-</tbody>
-<?php /**PATH D:\stk\stk-back\resources\views/components/table/body.blade.php ENDPATH**/ ?>
--- storage/framework/views/d60f5173ab444a51a9f998ddc38530ed.php
+++
@@ -1,5 +0,0 @@
-<thead class="bg-gray-50">
-    <?php echo e($slot); ?>
-
-</thead>
-<?php /**PATH D:\stk\stk-back\resources\views/components/table/head.blade.php ENDPATH**/ ?>
